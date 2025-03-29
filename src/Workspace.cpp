f2c39a97b28b4bf3bf860206b4d45431ebe97279
cowsed <richie.sommers@outlook.com>
2025-03-29 18:49:39 -0400

    remove unused graph demo, explicit cast

--- src/Workspace.cpp
+++ src/Workspace.cpp
@@ -45,7 +45,6 @@ void RouteData() {
   }
 }
 
-void Demo_RealtimePlots();
 void Init() { AddWidgetsToCollection(reg); }
 
 void OpenWidget(DefaultWidgetCreator widg_maker) {
@@ -145,52 +144,6 @@ void Draw() {
 }
 float to_radian(float deg) { return (deg * (3.14 / 180.0)); }
 static float knob_value = 0;
-
-void Demo_RealtimePlots() {
-  ImGui::BulletText("Move your mouse to change the data!");
-  ImGui::BulletText(
-      "This example assumes 60 FPS. Higher FPS requires larger buffer size.");
-  static ScrollingBuffer<ImVec2> sdata1, sdata2;
-  static RollingBuffer<ImVec2> rdata1, rdata2;
-  ImVec2 mouse = ImGui::GetMousePos();
-  static float t = 0;
-  t += ImGui::GetIO().DeltaTime;
-  sdata1.AddPoint(ImVec2{t, mouse.x * 0.0005f});
-  rdata1.AddPoint(ImVec2{t, mouse.x * 0.0005f});
-  sdata2.AddPoint(ImVec2{t, mouse.y * 0.0005f});
-  rdata2.AddPoint(ImVec2{t, mouse.y * 0.0005f});
-
-  static float history = 10.0f;
-  ImGui::SliderFloat("History", &history, 1, 30, "%.1f s");
-  // rdata1.Span = history;
-  // rdata2.Span = history;
-
-  static ImPlotAxisFlags flags =
-      ImPlotAxisFlags_NoTickLabels | ImPlotAxisFlags_NoTickMarks;
-
-  if (ImPlot::BeginPlot("Scrolling", ImVec2(-1, 250))) {
-    ImPlot::SetupAxes("Scrolling Title a", "Scrolling Title b", flags, flags);
-    ImPlot::SetupAxisLimits(ImAxis_X1, t - history, t, ImGuiCond_Always);
-    ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 1);
-    ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
-    ImPlot::PlotShaded("Mouse X", &sdata1.Data[0].x, &sdata1.Data[0].y,
-                       sdata1.Data.size(), -INFINITY, 0, sdata1.Offset,
-                       2 * sizeof(float));
-    ImPlot::PlotLine("Mouse Y", &sdata2.Data[0].x, &sdata2.Data[0].y,
-                     sdata2.Data.size(), 0, sdata2.Offset, 2 * sizeof(float));
-    ImPlot::EndPlot();
-  }
-  if (ImPlot::BeginPlot("Rolling", ImVec2(-1, 250))) {
-    ImPlot::SetupAxes(NULL, NULL, flags, flags);
-    ImPlot::SetupAxisLimits(ImAxis_X1, 0, history, ImGuiCond_Always);
-    ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 1);
-    ImPlot::PlotLine("Mouse X", &rdata1.Data[0].x, &rdata1.Data[0].y,
-                     rdata1.Data.size(), 0, 0, 2 * sizeof(float));
-    ImPlot::PlotLine("Mouse Y", &rdata2.Data[0].x, &rdata2.Data[0].y,
-                     rdata2.Data.size(), 0, 0, 2 * sizeof(float));
-    ImPlot::EndPlot();
-  }
-}
 
 } // namespace Workspace
 
--- src/widgets/ControlSystems/PidVizWidget.cpp
+++ src/widgets/ControlSystems/PidVizWidget.cpp
@@ -16,7 +16,7 @@ void PidVizWidget::ReceiveData(DataElement data) {
     t += 0.01;
     double sp = std::get<double>(data.value);
     // printf("I got %s: %f\n", data.path.toString().c_str(), sp);
-    sdata.AddPoint(ImVec2{t, sp});
+    sdata.AddPoint(ImVec2{(float)t, (float)sp});
   }
 }
 void PidVizWidget::Draw(bool *should_close) {
