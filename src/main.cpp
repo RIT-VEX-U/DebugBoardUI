e4bc2988d3d0855dda7318167be2b33740e22a27
cowsed <richie.sommers@outlook.com>
2025-04-03 12:00:19 -0400

    me when im dumb and confuse platform and workspace

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@
 #include <memory>
 #include <print>
 int main() {
-  Workspace::Init();
+  Platform::Data pdata = Platform::init("Debug Board UI");
   std::println("Init");
   // auto board =
   // std::make_shared<DebugBoardWebsocket>("ws://localhost:8080/ws");
   auto board = std::make_shared<DebugBoardWebsocket>("ws://10.115.37.28/ws");
   Workspace::AddSource(board);
+  Workspace::Init();
 
-  Platform::Data pdata = Platform::init("Debug Board UI");
 
   // Main loop
   while (!Platform::shouldclose(pdata)) {
